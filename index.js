3bbc73895ac1101bb96e8888350b1cf6ef3293cf
Shoaib Kalsekar <shoaibkalsekar@gmail.com>
2014-03-18 12:30:32 +0530

    Working code, refactored, only from console

--- index.js
+++ index.js
@@ -1,102 +1,173 @@
+// use strict;
+
+var Game = {
+
+  universe : undefined,
+
+  initializeUniverse : function($root, x, y){
+    // $root is the root html node
+    // 'x' is the no. of blocks on x-axis
+    // 'y' is the no. of blocks on y-axis
+
+    this.universe = {
+      rootNode : $root,
+      length : y,
+      height : x
+    }
+
+    $root.html('');
+
+    for(var i = 0; i<y; i++)
+    {
+      // create the rows
+      var $new_row = $("<tr></tr>"),
+          $new_col = "";
+
+      for(var j = 0; j<x; j++)
+      {
+        // create columns in each row
+        $new_col += "<td class='cell' data-isAlive='false' id=cell_"+i+"_"+j+"></td>";
+      }
+
+      $new_row.append($new_col);
+      $root.append($new_row);
+    };
+
+  },
+
+  seedLives : function($root, chance){
+    // $root is the grid html node
+    // chance is the probability of life in the universe
+    var universe = this.universe;
+    if(!universe)
+      throw 'Universe is not initialized';
+    for(var i=0; i<universe.length; i++)
+      for(var j=0; j<universe.height; j++)
+      {
+        $cell = this._getCell(i, j);
+        if(Math.random() > chance)
+          this.giveLife($cell);
+      }
+  },
+
+  _getCell : function(i, j){
+    return this.universe.rootNode.find('#cell_'+i+'_'+j);
+  },
+
+  giveLife : function($cell){
+    $cell.attr('data-isAlive', 'true');
+    $cell.addClass('live');
+  },
+
+  killCell : function($cell){
+    $cell.attr('data-isAlive', 'false');
+    $cell.removeClass('live');
+  },
+
+  startEvolution : function(interval){
+    this.isUniverse();
+    // this.generationInterval = setInterval('evolution', 500);
+    window.setInterval(this._evolve.bind(this), interval)
+    // function()
+  },
+
+  _evolve : function(){
+    this.createNextGeneration();
+    this.renderNextGeneration();
+  },
+
+  createNextGeneration : function(){
+    this.isUniverse();
+    var universe = this.universe,
+        $cell;
+    for(var i=0; i<universe.length; i++)
+      for(var j=0; j<universe.height; j++)
+      {
+        $cell = this._getCell(i,j);
+        if(this.isAlive(i,j))
+        {
+          if(this.getNeighbours(i,j).liveCount === 2 || this.getNeighbours(i,j).liveCount === 3)
+            $cell.attr('data-isAlive', 'true');
+          else
+            $cell.attr('data-isAlive', 'false');
+        }
+        else
+        {
+          if(this.getNeighbours(i,j).liveCount === 3)
+            $cell.attr('data-isAlive', 'true');
+        }
+      }
+  },
+
+  getNeighbours : function(i,j){
+    this.isUniverse();
+    var universe = this.universe,
+        isAlive = this.isAlive,
+        liveCount = 0,
+        deadCount = 0;
+
+    if(isAlive(i-1, j-1))
+      liveCount +=1;
+    if(isAlive(i-1, j))
+      liveCount +=1;
+    if(isAlive(i-1, j+1))
+      liveCount +=1;
+
+    if(isAlive(i, j-1))
+      liveCount +=1;
+    if(isAlive(i, j+1))
+      liveCount +=1;
+
+    if(isAlive(i+1, j-1))
+      liveCount +=1;
+    if(isAlive(i+1, j))
+      liveCount +=1;
+    if(isAlive(i+1, j+1))
+      liveCount +=1;
+
+    return {liveCount: liveCount, deadCount: 8-liveCount};
+  },
+
+  isAlive : function(i, j)
+  {
+    var universe = Game.universe;
+    if(i>= universe.length || j >= universe.height || i<0 || j<0)
+      return false;
+    return $("#cell_"+i+"_"+j).hasClass('live');
+  },
+
+  renderNextGeneration : function(){
+    this.isUniverse();
+    var universe = this.universe,
+        $cell;
+    for(var i=0; i<universe.length; i++)
+      for(var j=0; j<universe.height; j++)
+      {
+        $cell = $("#cell_"+i+"_"+j);
+        if($cell.attr('data-isAlive') === 'true')
+          this.giveLife($cell);
+        else
+          this.killCell($cell);
+      }
+  },
+
+  isUniverse : function(){
+    var universe = this.universe;
+    if(!universe)
+      throw 'Universe is not defined';
+  },
+
+  startLife : function($root, x, y){
+    this.initializeUniverse($root, x, y);
+    this.seedLives($root, 0.5);
+  }
+}
+
+
 $(function(){
 
+  $(".cell").click(function(){
+    Game.getLife(this);
+  });
 
-  var $gameContainer = $("#game_container"),
-      x = 30,
-      y = 40;
-
-  for(var i = 0; i<x; i++)
-  {
-    // create the rows
-    var $new_row = $("<tr></tr>"),
-        $new_col = "";
-
-    for(var j = 0; j<y; j++)
-    {
-      // create columns in each row
-      $new_col += "<td class='cell dead' id=cell_"+i+"_"+j+"></td>";
-    }
-
-    $new_row.append($new_col);
-    $gameContainer.append($new_row);
-  };
-  // console.log($table);
-
-  Game = function($gameContainer, timeDelay)
-  {
-    var game = setInterval(checkLives, timeDelay);
-
-    function checkLives()
-    {
-      for(var i=0; i<x; i++)
-      {
-        for(var j=0; j<y; j++)
-        {
-          var $current_cell = $("#cell_"+i+"_"+j);
-          if(compute_status($current_cell, x, y) === false)
-            $current_cell.removeClass('dead').addClass('live');
-          else
-            $current_cell.removeClass('live').addClass('dead');
-        }
-      }
-    }
-
-
-    function compute_status($a, b, c)
-    {
-      var id = $a.attr('id'),
-          i = parseInt(id.split("_")[1] , 10),
-          j = parseInt(id.split("_")[2] , 10),
-          n_live = 0,
-          n_dead = 0;
-
-      // console.log(i,j);
-
-      for(var p = -1; p<=1; p++)
-      {
-        for(var q = -1; q<=1; q++)
-        {
-          if( !(p === i && q === q))
-          {
-            var $cell = $("#cell_"+(i+p)+"_"+(j+q));
-            if($cell.attr('id'))
-            {
-              if($cell.hasClass('live'))
-                n_live += 1;
-              if($cell.hasClass('dead'))
-                n_dead += 1;
-            }
-          }
-        }
-      }
-
-      if($a.hasClass('live') && !$a.hasClass('dead'))
-      {
-        if(n_live < 2)
-          return false;
-        if(n_live === 2 || n_live === 3)
-          return true;
-        if(n_live > 3)
-          return false;
-      }
-
-      if($a.hasClass('dead') && !$a.hasClass('live'))
-      {
-        if(n_live === 3)
-          return true;
-      }
-    }
-  }
-
-  // Game($gameContainer, 300);
-
-  $(".cell").on('click', function(){
-    $this = $(this);
-    if($this.hasClass('live'))
-      $this.removeClass('live').addClass('dead');
-    else
-      $this.removeClass('dead').addClass('live');
-  })
-
-
-});
+})
